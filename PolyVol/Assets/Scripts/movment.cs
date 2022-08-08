5ca82c8a39ee12beca75ced2302171e191577fc5
Mo2a <98758389+Ahmed-Tarek-Abdellatif@users.noreply.github.com>
2022-08-09 00:55:28 +0200

    .

--- PolyVol/Assets/Scripts/movment.cs
+++ PolyVol/Assets/Scripts/movment.cs
@@ -11,4 +11,24 @@ public class movment : MonoBehaviour
 
     //REFERENCES
     private CharacterController controller;
+
+    private void start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float moveZ = Input.GetAxis("Vertical");
+
+        moveDirection = new Vector3 (0, 0, moveZ);
+        moveDirection *= walkSpeed;
+
+        controller.Move(moveDirection * Time.deltaTime);
+    }
 }
