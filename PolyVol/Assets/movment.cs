bf54d27b9665665ff47e9aefca702c7962d0742a
Mo2a <98758389+Ahmed-Tarek-Abdellatif@users.noreply.github.com>
2022-08-08 03:45:49 +0200

    movment bayza

--- PolyVol/Assets/movment.cs
+++ PolyVol/Assets/movment.cs
@@ -1,18 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class movment : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        float xDirection = Input.GetAxis("Horizontal");
+        float zDirection = Input.GetAxis("Vertical");
+
+        Vector3 MovmentDirection = new Vector3(xDirection, 0.0f, zDirection);
+        transform.position += MovmentDirection;
     }
 }
